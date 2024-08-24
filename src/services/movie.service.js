06f2588abad2a7b920319ab4a0d8c886f1cc56f5
Gustavo Santana <gustavosantlopes3@gmail.com>
2024-08-24 16:49:30 +0000

    movie service all

--- src/services/movie.service.js
+++ src/services/movie.service.js
@@ -2,6 +2,7 @@ import { goodfellas, popular } from '../../test/fixtures/movies.js'
 import { roles } from '../../test/fixtures/people.js'
 import { toNativeTypes } from '../utils.js'
 import NotFoundError from '../errors/not-found.error.js'
+import { int } from 'neo4j-driver'
 
 // TODO: Import the `int` function from neo4j-driver
 
@@ -39,12 +40,30 @@ export default class MovieService {
    */
   // tag::all[]
   async all(sort = 'title', order = 'ASC', limit = 6, skip = 0, userId = undefined) {
-    // TODO: Open an Session
-    // TODO: Execute a query in a new Read Transaction
-    // TODO: Get a list of Movies from the Result
-    // TODO: Close the session
+    // Open a new session
+    const session = this.driver.session()
+    // Execute a query in a new Read Transaction
+    const res = await session.executeRead(
+      tx => tx.run(
+        `
+        MATCH (m:Movie)
+        WHERE m.\`${sort}\` IS NOT NULL
+        RETURN m {
+          .*
+        } AS movie
+        ORDER BY m.\`${sort}\` ${order}
+        SKIP $skip
+        LIMIT $limit
+      `, { skip: int(skip), limit: int(limit) })
+    )
+    // Get a list of Movies from the Result
+    const movies = res.records.map(
+      row => toNativeTypes(row.get('movie'))
+    )
+    // Close the session
+    await session.close()
 
-    return popular
+    return movies
   }
   // end::all[]
 
